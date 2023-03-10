f3fd92f26438b8bc3991711b2a68749b24c1b8e8
John Cassidy <jpcassidy@outlook.com>
2023-03-10 07:25:21 -0500

    protobuf collections and maps

--- GrpcIndepthProtobuf/Services/GreeterService.cs
+++ GrpcIndepthProtobuf/Services/GreeterService.cs
@@ -9,9 +9,28 @@ namespace GrpcIndepthProtobuf.Services {
         }
 
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
-            return Task.FromResult(new HelloReply {
-                Message = "Hello " + request.Name
+            var message = new HelloReply {
+                Message = "Hello " + request.Name,
+                NestedMessageField = new HelloReply.Types.NestedMessage()
+            };
+
+            message.NestedMessageField.StringCollection.Add("entry 1");
+            message.NestedMessageField.StringCollection.Add(new List<string>
+            {
+                "entry 2",
+                "entry 3"
             });
+
+            message.NestedMessageField.StringToStringMap.Add("entry 1", "value 1");
+            message.NestedMessageField.StringToStringMap.Add(new Dictionary<string, string>
+            {
+                { "entry 2", "value 2" },
+                { "entry 3", "value 3" }
+            });
+
+            message.NestedMessageField.StringToStringMap["entry 4"] = "value 4";
+
+            return Task.FromResult(message);
         }
     }
 }
